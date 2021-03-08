26a2ae2fe6d4c1b06b7caef4b9d96b579d5fca4d
Brendan Kim <brendankim93@yahoo.com>
2021-03-07 22:10:33 -0800

    added files for next netlify repo

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,36 @@
-import styles from './Footer.module.css'
+import Link from "next/link"
 
-export default function Footer() {
-  return (
-    <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
-    </>
-  )
+export default function Footer () {
+    return (
+        <div className="footer">
+          <div className="footer-link">
+            <Link href="/contact?amp=1">
+                <a>Hit us up.</a>
+            </Link>     
+          </div>
+          <div>
+            <p className="footer-text">
+                2021 © || 201 Lab
+            </p>
+          </div>
+          <style jsx>{`
+            .footer {
+                background-color: black;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                padding: 8em;
+            }
+            a {
+                font-size: 3em; 
+                
+                color: white;
+            }
+            .footer-text {
+
+            }
+          `}
+          </style>
+        </div>
+    );
 }
